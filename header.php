39b267331cd43764475ed76d20b2e777bcc502b4
Harvey Zack <laozhaochaguan@hotmail.com>
2017-09-15 12:44:57 +0800

    update

--- header.php
+++ header.php
@@ -31,29 +31,34 @@ $logo = cs_get_option( 'i_logo' );
 		<!-- header 开始-->
 		<?php if (!is_mobile()) { ?>
 		  <header id="header" class="m_hide">
-				<div class="topbar clearfix">
-					<div class="skin fl clearfix">
-						<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin01_cloth.css" class="with-tooltip skin-cloth" data-tooltip="布质"></a>
-						<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin02_black.css" class="with-tooltip skin-black" data-tooltip="暗黑"></a>
-						<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin03_paper.css" class="with-tooltip skin-paper" data-tooltip="纸质"></a>
-						<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin04_steam.css" class="with-tooltip skin-steam" data-tooltip="朋克"></a>
+				<div class="header_inner">
+					<div class="topbar clearfix">
+						<div class="skin fl clearfix">
+							<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin01_cloth.css" class="with-tooltip skin-cloth" data-tooltip="布质"></a>
+							<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin02_black.css" class="with-tooltip skin-black" data-tooltip="暗黑"></a>
+							<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin03_paper.css" class="with-tooltip skin-paper" data-tooltip="纸质"></a>
+							<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin04_steam.css" class="with-tooltip skin-steam" data-tooltip="朋克"></a>
+						</div>
+						<div class="fr clearfix">
+							切换
+						</div>
 					</div>
-					<div class="fr clearfix">
-						切换
+					<div class="logo">
+						<a class="logo_img" href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo('name'); ?>">
+							<img src="<?php echo $logo; ?>" alt="<?php bloginfo('name'); ?>">
+						</a>
+						<p class="blog_name"><?php bloginfo('name'); ?></p>
 					</div>
+					<nav class="menu">
+						<?php wp_nav_menu(array(
+							'theme_location' => 'header',
+							'container' => 'div',
+							'depth' => 2,
+							'container_class' => 'wrapper',
+							'menu_class' => 'list clearfix'
+						)); ?>
+					</nav>
 				</div>
-
-				<div class="logo">
-					<a class="logo_img" href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo('name'); ?>">
-						<img src="<?php echo $logo; ?>" alt="<?php bloginfo('name'); ?>">
-					</a>
-					<p class="blog_name"><?php bloginfo('name'); ?></p>
-				</div>
-
-				<nav class="menu">
-					<?php wp_nav_menu(array('theme_location' => 'header', 'container' => 'div','depth' => 2, 'container_class' => 'wrapper', 'menu_class' => 'list clearfix')); ?>
-				</nav>
-
 			</header>
 			<!-- header 结束-->
 		<?php } ?>
