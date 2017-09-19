2701b07a8cf783abb68fedec424d5155961b0103
Harvey Zack <laozhaochaguan@hotmail.com>
2017-09-19 11:56:03 +0800

    update

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $pagination = cs_get_option('i_pagination');
                               if(!empty( $slider['i_slider_link'])){
                                 echo '<a href="'. $slider['i_slider_link'] .'"';
                               }
-                              if (!empty($slider['i_slider_link']) && $slider['i_slider_newtab'] == true) {
+                              if (!empty($slider['i_slider_link']) && $slider['i_slider_newtab']) {
                                 echo 'target="_black">';
                               } else {
                                 if (!empty( $slider['i_slider_link'])){
@@ -46,20 +46,47 @@ $pagination = cs_get_option('i_pagination');
 
         <!-- archive title 开始  -->
         <?php if(is_search()) { ?>
-          <h6 class="archive-title"><div class="title-inner"><?php printf( __( '搜索:  %s' ), '<span>' . get_search_query() . '</span>' ); ?></div></h6>
+          <h6 class="archive-title">
+            <div class="title-inner">
+              <?php printf( __( '搜索:  %s' ), '<span>' . get_search_query() . '</span>' ); ?>
+            </div>
+          </h6>
         <?php } else if(is_tag()) { ?>
-          <h6 class="archive-title"><div class="title-inner">标签： <?php single_tag_title(); ?></div></h6>
+          <h6 class="archive-title">
+            <div class="title-inner">
+              标签： <?php single_tag_title(); ?>
+            </div>
+          </h6>
         <?php } else if(is_day()) { ?>
-          <h6 class="archive-title"><div class="title-inner">日期： <?php _e('归档'); ?> <?php echo get_the_date(); ?></div></h6>
+          <h6 class="archive-title">
+            <div class="title-inner">
+              日期： <?php _e('归档'); ?> <?php echo get_the_date(); ?>
+            </div>
+          </h6>
         <?php } else if(is_month()) { ?>
-          <h6 class="archive-title"><div class="title-inner">日期： <?php echo get_the_date('F Y'); ?></div></h6>
+          <h6 class="archive-title">
+            <div class="title-inner">
+              日期： <?php echo get_the_date('F Y'); ?>
+            </div>
+          </h6>
         <?php } else if(is_year()) { ?>
-          <h6 class="archive-title"><div class="title-inner">日期： <?php echo get_the_date('Y'); ?></div></h6>
+          <h6 class="archive-title">
+            <div class="title-inner">
+              日期： <?php echo get_the_date('Y'); ?>
+            </div>
+          </h6>
         <?php } else if(is_category()) { ?>
-          <h6 class="archive-title"><div class="title-inner">分类： <?php single_cat_title(); ?></div></h6>
+          <h6 class="archive-title">
+            <div class="title-inner">
+              分类： <?php single_cat_title(); ?>
+            </div>
+          </h6>
         <?php } else if(is_author()) { ?>
-          <h6 class="archive-title"><div class="title-inner">作者： <?php
-          $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); echo $curauth->display_name; ?></div></h6>
+          <h6 class="archive-title">
+            <div class="title-inner">
+              作者： <?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); echo $curauth->display_name; ?>
+            </div>
+          </h6>
         <?php } ?>
         <!-- archive title 结束  -->
 
@@ -67,50 +94,62 @@ $pagination = cs_get_option('i_pagination');
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <?php setPostViews(get_the_ID());?>
           <article <?php post_class('index-post'); ?>>
-          <?php if (!is_mobile()) { ?>
-                <div class="post-tool m_hide">
-                  <?php if ($date == true) { ?>
-                    <!-- 日期  -->
-                    <div class="post-date">
-                      <span class="date-month"><?php the_time('m'); ?>月</span>
-                      <span class="date-day"><?php the_time('d'); ?></span>
-                      <span class="date-year"><?php the_time('Y'); ?></span>
-                    </div>
-                  <?php } ?>
-                  <?php if ($view == true) { ?>
-                    <!-- 浏览  -->
-                    <div class="post-view">
-                      <a href="javascript:void(0)">
-                        <i class="fa fa-eye"></i><span class="view-num"><?php echo getPostViews(get_the_ID()); ?></span>
-                      </a>
-                    </div>
-                  <?php } ?>
-                  <?php if(current_user_can('level_10')){  ?>
-                    <!-- 编辑  -->
-                    <div class="post-edit">
-                        <?php edit_post_link( __( '<i class="fa fa-edit"></i><span class="view-num">编辑</span>' ), '<div class="edit-link">', '</div>' ); ?>
-                    </div>
-                  <?php } ?>
-                </div>
-          <?php } ?>
-
+            <?php if (!is_mobile()) { ?>
+              <div class="post-tool m_hide">
+                <?php if ($date) { ?>
+                  <!-- 日期  -->
+                  <div class="post-date">
+                    <span class="date-month"><?php the_time('m'); ?>月</span>
+                    <span class="date-day"><?php the_time('d'); ?></span>
+                    <span class="date-year"><?php the_time('Y'); ?></span>
+                  </div>
+                <?php } ?>
+                <?php if ($view) { ?>
+                  <!-- 浏览  -->
+                  <div class="post-view">
+                    <a href="javascript:void(0)">
+                      <i class="fa fa-eye"></i><span class="view-num"><?php echo getPostViews(get_the_ID()); ?></span>
+                    </a>
+                  </div>
+                <?php } ?>
+                <?php if(current_user_can('level_10')){  ?>
+                  <!-- 编辑  -->
+                  <div class="post-edit">
+                      <?php edit_post_link( __( '<i class="fa fa-edit"></i><span class="view-num">编辑</span>' ), '<div class="edit-link">', '</div>' ); ?>
+                  </div>
+                <?php } ?>
+              </div>
+            <?php } ?>
             <div class="post-wrap">
-
               <?php get_template_part('format', 'standard'); ?>
-
               <?php if (!is_page()) { ?>
                 <ul class="post-meta clearfix">
-                  <?php if ($cat == true) { ?>
-                    <li class="mate-cat fl clearfix"><i class="fa fa-bookmark"></i><?php the_category(' '); ?></li>
+                  <?php if ($cat) { ?>
+                    <li class="mate-cat fl clearfix">
+                      <i class="fa fa-bookmark"></i>
+                      <?php the_category(' '); ?>
+                    </li>
                   <?php } ?>
-                  <?php if ($tag == true) { ?>
-                    <?php $posttags = get_the_tags(); if ($posttags) { ?><li class="meta-tabs fl clearfix m_hide"><i class="fa fa-tags"></i><?php the_tags('', ' ', ''); ?></li><?php } ?>
+                  <?php if ($tag) { ?>
+                    <?php $posttags = get_the_tags(); if ($posttags) { ?>
+                      <li class="meta-tabs fl clearfix m_hide">
+                        <i class="fa fa-tags"></i>
+                        <?php the_tags('', ' ', ''); ?>
+                      </li>
+                    <?php } ?>
                   <?php } ?>
-                  <?php if ($like == true) { ?>
-                    <li class="meta-like fr mr0"><?php echo getPostLikeLink( get_the_ID() ); ?></li>
+                  <?php if ($like) { ?>
+                    <li class="meta-like fr mr0">
+                      <?php echo getPostLikeLink( get_the_ID() ); ?>
+                    </li>
                   <?php } ?>
-                  <?php if ($com == true) { ?>
-                    <li class="mate-com fr"><i class="fa fa-comments-o"></i><span class="mate-num"><?php comments_number(__('0','island'),__('1','island'),__( '%','island') );?></span></li>
+                  <?php if ($com) { ?>
+                    <li class="mate-com fr">
+                      <i class="fa fa-comments-o"></i>
+                      <span class="mate-num">
+                        <?php comments_number(__('0','island'),__('1','island'),__( '%','island') );?>
+                      </span>
+                    </li>
                   <?php } ?>
                 </ul>
               <?php } ?>
@@ -121,25 +160,25 @@ $pagination = cs_get_option('i_pagination');
         </div>
 
         <?php if ( $pagination == 'i_ajax') { ?>
-            <?php if( island_page_has_nav() ) : ?>
-                <div class="post-nav">
-                    <div class="post-nav-inside text-c clearfix">
-                        <div class="post-nav-left"><?php previous_posts_link(__('上一页')) ?></div>
-                        <div class="post-nav-right"><?php next_posts_link(__('下一页')) ?></div>
-                    </div>
-                </div>
-                <?php endif; ?>
-            <?php } else { ?>
-            <div class="posts-nav text-c">
-              <div class="nav-inside clearfix">
-                <?php echo paginate_links(array(
-                    'prev_text'          =>'<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-                    'next_text'          =>'<i class="fa fa-chevron-right" aria-hidden="true"></i>',
-                    'before_page_number' => '',
-                    'mid_size'           => 2,
-                ));?>
+          <?php if( island_page_has_nav() ) : ?>
+            <div class="post-nav">
+              <div class="post-nav-inside text-c clearfix">
+                <div class="post-nav-left"><?php previous_posts_link(__('上一页')) ?></div>
+                <div class="post-nav-right"><?php next_posts_link(__('下一页')) ?></div>
               </div>
             </div>
+            <?php endif; ?>
+          <?php } else { ?>
+          <div class="posts-nav text-c">
+            <div class="nav-inside clearfix">
+              <?php echo paginate_links(array(
+                'prev_text'          =>'<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+                'next_text'          =>'<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+                'before_page_number' => '',
+                'mid_size'           => 2,
+              ));?>
+            </div>
+          </div>
         <?php } ?>
 
 <?php get_footer(); ?>
