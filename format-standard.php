7b27a05d678353b18510e6c5b1d3db9493d73362
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-09-19 19:29:11 +0800

    update

--- format-standard.php
+++ format-standard.php
@@ -26,6 +26,7 @@
       <?php if (has_post_thumbnail()) { ?>
         <div class="post-featured" >
           <a href="<?php the_permalink(); ?>#content" title="<?php the_title(); ?>">
+            <?php wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) ) ?>
             <?php the_post_thumbnail( 'thumbnail' ); ?>
           </a>
         </div>
