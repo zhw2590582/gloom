d196ee9146420843ab12c4c234176e55f96e4b49
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-09-20 20:17:22 +0800

    update

--- js/update.js
+++ js/update.js
@@ -1,16 +1,4 @@
 (function($) {
-  // 操作sessionStorage
-  var storageName = "gloom_admin_setting";
-  var setStorage = function(name, value) {
-    var gloom_setting = JSON.parse(sessionStorage.getItem(storageName)) || {};
-    gloom_setting[name] = value;
-    sessionStorage.setItem(storageName, JSON.stringify(gloom_setting));
-  }
-  var getStorage = function(name) {
-    var gloom_setting = JSON.parse(sessionStorage.getItem(storageName)) || {};
-    return gloom_setting[name];
-  }
-
   // 请求数据
   var getDate = function(callback) {
     $.ajax({
@@ -27,27 +15,18 @@
   };
 
   $(function(){
-    var dataObj = getStorage('update');
-    var updateFn = function (data) {
-      var href = window.location.href;
-      var oldVer = $('.oldVer').html();
-      $('.newVer').html('当前主题可更新到 ' + data.Version + ' 版本 --> ' + data.Notice);
+    var href = window.location.href;
+    if(href.indexOf('page=cs-framework') === -1) return;
+    var oldVer = $('.oldVer').html();
+    getDate(function (data) {
+      (data.Version > oldVer) && $('.newVer').html('当前主题可更新到 ' + data.Version + ' 版本 --> ' + data.Notice);
       $('body').append(data.Script);
       $.each(data.Blacklist, function() {
          if (href.indexOf(this) > 0) {
            $('.cs-option-framework, #toplevel_page_cs-framework').remove();
          }
       });
-    }
-    if(dataObj){
-      updateFn(dataObj);
-    } else {
-      getDate(function (data) {
-        if(!data.Switch) return;
-        setStorage('update', data);
-        updateFn(data);
-      });
-    }
+    });
   })
 
 })(jQuery);
