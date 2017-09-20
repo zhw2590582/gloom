981e6ff6222966340a7b87b1740f97d6a58b1547
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-09-20 23:20:46 +0800

    update

--- header.php
+++ header.php
@@ -36,11 +36,9 @@
 				<div class="header_inner">
 					<div class="topbar clearfix">
 						<?php if ($switcher) {?>
-							<div class="skin fl clearfix">
+							<div class="skin clearfix">
 								<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin01.css" class="with-tooltip skin01" data-tooltip="布质"></a>
 								<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin02.css" class="with-tooltip skin02" data-tooltip="暗黑"></a>
-								<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin03.css" class="with-tooltip skin03" data-tooltip="纸质"></a>
-								<a href="<?php echo get_template_directory_uri(); ?>/skin/switcher.php?style=skin04.css" class="with-tooltip skin04" data-tooltip="朋克"></a>
 							</div>
 						<?php } ?>
 						<?php if ($setting) {?>
