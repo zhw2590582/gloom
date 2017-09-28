0fdc361b7dfa7a755e0786162dcf564d1f6fae0b
Harvey Zack <laozhaochaguan@hotmail.com>
2017-09-28 11:49:04 +0800

    update

--- cs-framework-override/config/framework.config.php
+++ cs-framework-override/config/framework.config.php
@@ -75,7 +75,7 @@ $options[] = array(
             'title' => '自定义皮肤',
             'class' => 'horizontal',
             'options' => array(
-                'skin01' => '淡雅',
+                'skin01' => '淡雅',                
                 'skin02' => '致郁',
             ) ,
             'default' => 'skin01'
